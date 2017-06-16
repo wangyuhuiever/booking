e62d4078e30a372fbf5e73d21a51a00b5aea1ecc
wangyuhuiever <wangyuhuiever@163.com>
2017-06-16 19:47:59 +0800

    增加验证金额大写。

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,16 +1,18 @@
 #! -*- encoding:utf-8 -*-
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, SelectField, BooleanField
-from wtforms.validators import Required, Length
+from wtforms.validators import Required, Length, Regexp
 
 class DataForm(FlaskForm):
-    number = StringField("金额", validators=[Required(), Length(0, 8)])
+    number = StringField("金额", validators=[Required(), Length(0, 8, '最大位数为8'), Regexp('^[1-9][0-9]*$',0,
+                                                                             '金额必须为正整数')])
     leixing = SelectField("借贷", choices=[(None, '请选择'), ('支出', '支出'), ('收入', '收入')])
     message = StringField("备注")
     submit = SubmitField("记账")
 
 class ChangeDataForm(FlaskForm):
-    number = StringField("金额", validators=[Required(), Length(0, 8)])
+    number = StringField("金额", validators=[Required(), Length(0, 8, '最大位数为8'), Regexp('^[1-9][0-9]*$',0,
+                                                                             '金额必须为正整数')])
     leixing = SelectField("借贷", choices=[(None, '请选择'), ('支出', '支出'), ('收入', '收入')])
     message = StringField("备注")
     delete = BooleanField("删除")
