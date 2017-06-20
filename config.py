d631b22556eaf212af06b2a614a8f6b8e1c723ec
wangyuhuiever <wangyuhuiever@163.com>
2017-06-20 14:35:22 +0800

    email config

--- config.py
+++ config.py
@@ -6,8 +6,8 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'cheer up'
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    MAIL_SERVER = 'smtp.163.com'
-    MAIL_PORT = 25
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
